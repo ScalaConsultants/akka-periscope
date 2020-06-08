612db3eb090f27231c17666de8c9c6a2ab8314cb
Vladimir Pavkin <vpavkin@gmail.com>
2020-06-08 11:55:22 +0300

    Add a doc note

--- core/src/main/scala/io/scalac/periscope/common/Deque.scala
+++ core/src/main/scala/io/scalac/periscope/common/Deque.scala
@@ -1,5 +1,9 @@
 package io.scalac.periscope.common
 
+/**
+Cross-compilation helper, abstracting over a specific queue implementation.
+Allows to use a more performant ArrayDeque in Scala 2.13.
+ */
 trait Deque[A] {
   def toVector: Vector[A]
   def removeLast: A
